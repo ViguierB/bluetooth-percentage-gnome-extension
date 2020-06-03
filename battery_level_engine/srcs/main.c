af0f0d31acf86fa5dd3a22897b6944b65d374cef
Benjamin Viguier <ben@holidev.net>
2020-06-03 17:53:12 +0000

    Fix imports

--- battery_level_engine/srcs/main.c
+++ battery_level_engine/srcs/main.c
@@ -1,6 +1,6 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "stdint.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 void*       create_battery_level_engine();
 int         delete_battery_level_engine(void*);
