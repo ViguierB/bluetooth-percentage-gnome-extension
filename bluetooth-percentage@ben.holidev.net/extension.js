8744d3709dd740da2ce6f9e52d7feedea1483e67
Ben <ben@holidev.net>
2020-06-29 15:46:05 +0200

    [feat] change icons in menu

--- bluetooth-percentage@ben.holidev.net/extension.js
+++ bluetooth-percentage@ben.holidev.net/extension.js
@@ -46,74 +46,10 @@ function init() {
   instance = new bluetooth_battery_level_extention(bluetooth_menu);
 }
 
-// function enable() {
-//   instance.enable();
-// }
-
-// function disable() {
-//   instance.disable();
-// }
-
-const PanelMenu = imports.ui.panelMenu;
-const Config = imports.misc.config;
-const SHELL_MINOR = parseInt(Config.PACKAGE_VERSION.split('.')[1]);
-const GObject = imports.gi.GObject;
-const St = imports.gi.St;
-const Gio = imports.gi.Gio;
-
-var ExampleIndicator = class ExampleIndicator extends PanelMenu.Button {
-
-    _init() {
-        super._init(0.0, `${Me.metadata.name} Indicator`, false);
-
-        this.theme_node = this.get_theme_node();
-        const colors = this.theme_node.get_icon_colors();
-
-        // Pick an icon
-        let icon = new St.Icon({
-            style_class: 'system-status-icon'
-        });
-        icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/bluetooth-battery-90-symbolic.svg`);
-        this.actor.add_child(icon);
-
-        // Add a menu item
-        this.menu.addAction('Menu Item', this.menuAction, null);
-    }
-
-    menuAction() {
-        log('Menu item activated');
-    }
-}
-
-// Compatibility with gnome-shell >= 3.32
-if (SHELL_MINOR > 30) {
-    ExampleIndicator = GObject.registerClass(
-        {GTypeName: 'ExampleIndicator'},
-        ExampleIndicator
-    );
-}
-
-var indicator = null;
-
 function enable() {
-    logger.log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
-
-    indicator = new ExampleIndicator();
-
-    // The `main` import is an example of file that is mostly live instances of
-    // objects, rather than reusable code. `Main.panel` is the actual panel you
-    // see at the top of the screen.
-    Main.panel.addToStatusArea(`${Me.metadata.name} Indicator`, indicator);
+  instance.enable();
 }
-
 
 function disable() {
-    logger.log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
-
-    // REMINDER: It's required for extensions to clean up after themselves when
-    // they are disabled. This is required for approval during review!
-    if (indicator !== null) {
-        indicator.destroy();
-        indicator = null;
-    }
+  instance.disable();
 }
--- bluetooth-percentage@ben.holidev.net/srcs/battery_level_extention.js
+++ bluetooth-percentage@ben.holidev.net/srcs/battery_level_extention.js
@@ -19,6 +19,17 @@ class _bluetooth_battery_level_extention extends signals {
     this._menu = this._bluetooth_indicator._item.menu;
     this._bt_level = new bluetooth_battery_level_engine();
     this._enabled = false;
+    this.icons = {
+      geticon: (level) => {
+        level = level - (level % 10);
+        return this.icons._cache[level / 10];
+      },
+      _cache: []
+    };
+    for (let i = 1; i <= 10; ++i) {
+      this.icons._cache[i] = Gio.icon_new_for_string(`${Me.path}/icons/bluetooth-battery-level-${i}0-symbolic.svg`);
+    }
+    
   }
 
   _override_bl_menu_label_text_member() {
@@ -42,10 +53,7 @@ class _bluetooth_battery_level_extention extends signals {
     this.register_signal(this._bt_level, 'battery_level_change', (_source, { device, lvl }) => {
       logger.log('got signal "battery_level_change" from ' + device.name + ': ' + lvl);
       this._bluetooth_indicator._item.label.text = `${device.name} (${lvl} %)`;
-      logger.log("icon path = " + `${Me.path}/icons/bluetooth-battery-${lvl}-symbolic.svg`);
-      let gicon = Gio.icon_new_for_string(`${Me.path}/icons/bluetooth-battery-${lvl}-symbolic.svg`)
-      logger.log(gicon);
-      // this._bluetooth_indicator._indicator = this._bluetooth_indicator._addIndicator();
+      let gicon = this.icons.geticon(lvl);
       this._bluetooth_indicator._indicator.gicon = gicon;
       this._bluetooth_indicator._item.icon.gicon = gicon;
       logger.log();
